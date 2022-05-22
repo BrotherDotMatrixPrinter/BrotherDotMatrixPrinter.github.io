0295940495ef39f0420a54985a5152643e75de58
BrotherDotMatrixPrinter <98490876+BrotherDotMatrixPrinter@users.noreply.github.com>
2022-05-22 18:43:19 -0400

    small update

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react'
+import UserData from './model/UserData.js'
+import TotalData from './model/TotalData.js'
+import PriceData from './model/PriceData.js'
+
+import UserInput from './component/UserInput.jsx'
+import Totals from './component/Totals.jsx'
+import Prices from './component/Prices.jsx'
+
+const App = () => {
+
+  const [ userData, setUserData ] = useState( new UserData() ),
+    [ totalData, setTotalData ] = useState( new TotalData( userData ) ),
+    [ priceData, setPriceData ] = useState( new PriceData() )
+
+  useEffect( () => {
+    setTotalData( new TotalData( userData ) )
+  }, [ userData ] )
+
+  useEffect( () => {
+    priceData.getData( setPriceData )
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [] )
+
+  /*
+  So we want it to go like this:
+  User Inputs
+  Reward/Tax/Fee/Cost Totals
+  */
+  return <div id = 'App'>
+
+    <UserInput
+      userData = { userData }
+      setUserData = { setUserData } />
+
+    <Totals totalData = { totalData } />
+
+    <Prices priceData = { priceData } />
+
+    {/* This is just a cheap fix for vertical scrolling */}
+    <div style = { { lineHeight: '0px' } }>&nbsp;</div>
+
+  </div>
+
+}
+
+export default App
---
+++ src/model/UserData.js
@@ -0,0 +1,14 @@
+class UserData {
+	tierOne = 0
+	tierTwo = 0
+	tierThree = 0
+	tierFour = 0
+	taxNft = false
+
+	/** @type { 'none' | 'bronze' | 'silver' | 'gold' } */
+	boostNft = 'none'
+
+	daysUntilClaim = 1
+}
+
+export default UserData
