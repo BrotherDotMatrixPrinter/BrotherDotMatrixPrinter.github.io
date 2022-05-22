16305fe05bf4f95be8515d57ee722059c35c17bf
BrotherDotMatrixPrinter <98490876+BrotherDotMatrixPrinter@users.noreply.github.com>
2022-05-22 18:15:21 -0400

    Delete src directory

--- src/class/BoostNfts.js
+++
@@ -1,5 +0,0 @@
-export default class BoostNfts {
-	static bronze = 0.02
-	static silver = 0.05
-	static gold = 0.10
-}
--- src/util/Constants.js
+++
@@ -1,5 +0,0 @@
-export default {
-	croUsdCGUrl: 'https://api.coingecko.com/api/v3/simple/price?ids=crypto-com-chain&vs_currencies=usd&include_market_cap=false&include_24hr_vol=false&include_24hr_change=false&include_last_updated_at=false',
-	crnUsdCGUrl: 'https://api.coingecko.com/api/v3/simple/price?ids=cronodes&vs_currencies=usd&include_market_cap=false&include_24hr_vol=false&include_24hr_change=false&include_last_updated_at=false',
-	v2NodeManagementContractAddress: '0x523CE472D66FC8001C2C78D6A1c7d88e93Ab1dDC'
-}
