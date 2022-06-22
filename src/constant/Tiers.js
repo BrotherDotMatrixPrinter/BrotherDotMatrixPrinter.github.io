1fc6325764796ff7999435c17a61e6f01a201ff0
BrotherDotMatrixPrinter <developerloganprunty@gmail.com>
2022-06-22 18:10:16 -0400

    update taxes

--- src/constant/Tiers.js
+++ src/constant/Tiers.js
@@ -8,19 +8,19 @@ const Tiers = {
 	two: {
 		crnCostPerNode: 20.00,
 		crnRewardPerDay: 0.18,
-		monthlyCROFee: 8.00
+		monthlyCROFee: 12.00
 	},
 
 	three: {
 		crnCostPerNode: 55.00,
 		crnRewardPerDay: 0.57,
-		monthlyCROFee: 20.00
+		monthlyCROFee: 24.00
 	},
 
 	four: {
 		crnCostPerNode: 110.00,
 		crnRewardPerDay: 1.20,
-		monthlyCROFee: 40.00
+		monthlyCROFee: 48.00
 	}
 }
 
